22f5b7032b7b33934ed1c6bdcf3ce902dd42086d
Mary Lou D. Cortez <maryloucortez12@gmail.com>
2024-05-16 09:51:19 +0800

    mobile view not yet done

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Management</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+<body>
+    @include('partials.sidebar') 
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+</body>
+</html>
